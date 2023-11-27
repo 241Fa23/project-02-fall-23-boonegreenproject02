50e2ce7d838a24b8a0b1d7e30b74ecb6d8f30b5a
elisegreen <s540473@nwmissouri.edu>
2023-11-27 11:38:53 -0600

    made comments to partners comments
    letting him know of the two methods decreaseHealth and increaseScore in the abstract class

--- src/project02startingfiles/Project02StartingFiles.java
+++ src/project02startingfiles/Project02StartingFiles.java
@@ -121,8 +121,8 @@ public class Project02StartingFiles {
             System.out.println("***********************************");
             //call up special move of character class to use
             //player has 60% chance to win battle
-            //if they lose the battle, player health -= 1 //I created method decreaseScore
-            //if they win the battle, player score += 2 //I created increaseScore
+            //if they lose the battle, player health -= 1 //I created method decreaseHealth
+            //if they win the battle, player score += 2 //I created increaseScore method
         } else if (choice.equals("r")) {
             success = run.nextInt(2);
             if (success == 0) {
